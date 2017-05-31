e8fd96a19aee23808150b49a2aa5a36cc415c94f
Pedro <plazari96@gmai.com>
2017-05-31 02:56:43 -0300

    inserindo umas mudanças de pastas e CSS

--- js/init.js
+++ js/init.js
@@ -5,13 +5,15 @@
       var item = items[Math.floor(Math.random()*items.length)];
 
       if(item == 'Overwatch'){
-          $('#video').attr('src', 'assts/Overwatch.mp4');
+          $('#video').attr('src', 'assts/videos/Overwatch.mp4');
           $('#custom').attr('href', 'css/Overwatch.css');
+          $('#game-logo').attr('src', 'assts/img/overwatch.png');
       }
 
       if(item == 'lol') {
-          $('#video').attr('src', 'assts/lol.mp4');
+          $('#video').attr('src', 'assts/videos/lol.mp4');
           $('#custom').attr('href', 'css/lol.css');
+          $('#game-logo').attr('src', 'assts/img/lol.png');
       }
 
 
